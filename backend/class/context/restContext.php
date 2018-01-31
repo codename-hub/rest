85da5fd410020faad4f18555d2cf87e210724150
Kevin Dargel <kevin@jocoon.de>
2018-01-31 10:04:23 +0100

    added restContext for providing structured access design via functions for REST-Standard (HTTP-Methods)

---
+++ backend/class/context/restContext.php
@@ -0,0 +1,93 @@
+<?php
+namespace codename\rest\context;
+
+use codename\core\context;
+
+/**
+ * abstract REST-Context
+ * just provides a function-based access to override HTTP-Method Responses
+ */
+abstract class restContext extends context implements restContextInterface
+{
+  /**
+   *
+   */
+  public function __construct()
+  {
+    // reset response data
+    // this is a data-only context
+    $this->getResponse()->reset();
+  }
+
+  /**
+   * [view_default description]
+   * @return [type] [description]
+   */
+  public function view_default () {
+    // empty
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_get()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_head()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_post()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_put()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_delete()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_trace()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_options()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function method_patch()
+  {
+    throw new \LogicException('Not implemented'); // TODO
+  }
+}
